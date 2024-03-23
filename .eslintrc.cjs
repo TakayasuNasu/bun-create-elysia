2d8ce5fc25677caa9aca88c6dc6d3b5bc7a38e43
takayasu.nasu <taka.beckham@gmail.com>
2024-03-23 07:50:59 +0000

    Add some config files

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+module.exports = {
+  ignorePatterns: ['*.d.ts'],
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:import/typescript', 'prettier'],
+  plugins: ['@typescript-eslint', 'import', 'unused-imports'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent'],
+          ['sibling', 'index'],
+        ],
+        pathGroups: [
+          {
+            pattern: '{bun,bun:*}',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '{elysia,@elysiajs/*}',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+  },
+}
